a8ffb7de77c56058017bf116dd6577b7a62f8ab3
Michael Braun <michaelrbraun1985@gmail.com>
2020-01-01 22:43:53 -0800

    Added hover states to generated cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,26 @@ import supervisor from "./icon-supervisor.svg"
 
 
 function Card(props) {
+    const [isHovered, setHover] = React.useState(false);
+    
+    const hoveredColor = "#ededed";
+    
+    function handleHover() {
+        setHover(true);
+        
+    }
+
+    function handleOut() {
+        setHover(false);
+    }
+    
+    
     return (
-    <div className={props.nameClass}>
+    <div 
+    onMouseOver={handleHover} 
+    onMouseOut={handleOut} 
+    style={{backgroundColor: isHovered ? hoveredColor : "white"}} 
+    className={props.nameClass}>
         <h3>{props.headline}</h3>
         <p>{props.descriptionText}</p>
         <img src={props.image} alt={props.alt} />
