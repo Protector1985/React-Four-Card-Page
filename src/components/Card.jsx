20bd74c224764d15302745db422904f4d18e9383
Michael Braun <michaelrbraun1985@gmail.com>
2020-01-02 23:18:48 -0800

    Utilized React Spring to animate hover effect over Cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import supervisor from "./icon-supervisor.svg"
+import {useSpring, animated} from "react-spring";
 
 
 function Card(props) {
     const [isHovered, setHover] = React.useState(false);
     
-    const hoveredColor = "#ededed";
+    const animation = useSpring({
+        backgroundColor: isHovered ? "#ededed" : "white"
+    });
+    
+
     
     function handleHover() {
         setHover(true);
@@ -16,18 +20,19 @@ function Card(props) {
         setHover(false);
     }
     
-    
+  
     return (
-    <div 
+    <animated.div 
     onMouseOver={handleHover} 
     onMouseOut={handleOut} 
-    style={{backgroundColor: isHovered ? hoveredColor : "white"}} 
+    style={animation}
     className={props.nameClass}>
         <h3>{props.headline}</h3>
         <p>{props.descriptionText}</p>
         <img src={props.image} alt={props.alt} />
-        
-    </div>
+
+    </animated.div> 
+    
     )
 }
 
